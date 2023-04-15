838dbdaeebffc44b86ecfe61a9abb4449a5dad86
Muslim shah <smuslimshah7@gmail.com>
2023-04-15 17:34:01 +0500

    code refactoring

--- expressBookReviews/final_project/index.js
+++ expressBookReviews/final_project/index.js
@@ -11,6 +11,12 @@ app.use(express.json());
 app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
 app.use("/customer/auth*", function auth(req, res, next) {
+    /**
+     * first of all it checks if the session exists 
+     * if exists it takes the token and verify it and
+     * add user to the request object
+     * and if  it does not exist it prompt the user to login first
+     */
     if (req.session.authorization) {
         const token = req.session.authorization['accessToken']; //gets the token
         const verifiedStatus = jwt.verify(token, `${process.env.jwtSecretKey}`);
